7604241b4debd5eaf262f61aa9ca1baa0329b0c0
MohamedElgamal <mohammed.elgamal@bibalex.org>
2024-08-15 16:09:58 +0300

    add bootstrap cdn

--- accountants/forms.py
+++ accountants/forms.py
@@ -7,6 +7,11 @@ class PersonsForm(forms.ModelForm):
         model = Persons
         exclude = ["created_at"]
 
+    def __init__(self, *args, **kwargs):
+        super(PersonsForm, self).__init__(*args, **kwargs)
+        self.fields["notes"].required = False
+        self.fields["suspend_reason"].required = False
+
 
 class CountriesForm(forms.ModelForm):
     class Meta:
